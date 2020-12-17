6267196a567bc1796a1786e25ab25bddc3d31df8
Pablo Trabajos <hz302783@hertz.net>
2020-12-17 14:48:33 +0000

    Initial Commit

---
+++ SwiftUIcameraTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIcameraTest
+//
+//  Created by Pablo Tamayo on 17/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIcameraTest/SwiftUIcameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIcameraTestApp.swift
+//  SwiftUIcameraTest
+//
+//  Created by Pablo Tamayo on 17/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIcameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
